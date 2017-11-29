7798e0fca3b8da6c66971f97dcbaf537c9d7a191
mattlink <mattlink570@gmail.com>
2017-11-29 09:42:03 -0600

    added SpringMVC structure

---
+++ spring-app/src/main/java/hello/PageController.java
@@ -0,0 +1,24 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageController {
+
+    private static final String template = "%s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/page")
+    public Person person(@RequestParam(value="name", defaultValue="no name given") String name) {
+        // return "page";
+        Person person1 = new Person(counter.incrementAndGet(),
+                            String.format(template, name),
+                            false);
+
+        return person1;
+    }
+
+}
---
+++ spring-app/src/main/java/hello/Person.java
@@ -0,0 +1,25 @@
+package hello;
+
+public class Person {
+    private final long id;
+    private final String name;
+    private final boolean isFemale;
+
+    public Person(long id, String name, boolean isFemale) {
+        this.id = id;
+        this.name = name;
+        this.isFemale = isFemale;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean getIsFemale(){
+        return isFemale;
+    }
+}
