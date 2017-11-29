9081291d32a6afa299cfb0dd746f40751cffb114
mattlink <mattlink570@gmail.com>
2017-11-29 16:38:03 -0600

    moved files around

--- spring-app/src/main/java/sacs/Model/Class.java
+++ spring-app/src/main/java/sacs/Model/Class.java
@@ -8,7 +8,7 @@ public class Class {
 	final private int chaMod;
 	final private Feat[] feats;
 	final private int lvl;
-	
+
 	public Class(int s, int d, int c, int i, int w, int ch, Feat f, int l) {
 		strMod = s;
 		dexMod = d;
@@ -19,39 +19,39 @@ public class Class {
 		feats[] = f;
 		lvl = l;
 	}
-	
+
 	public int getStrMod() {
 		return strMod;
 	}
-	
+
 	public int getDexMod() {
 		return dexMod;
 	}
-	
+
 	public int getConMod() {
 		return conMod;
 	}
-	
+
 	public int getIntMod() {
 		return intMod;
 	}
-	
+
 	public int getWisMod() {
 		return wisMod;
 	}
-	
+
 	public int getChaMod() {
 		return chaMod;
 	}
-	
+
 	public Feat[] getFeat() {
 		return feats;
 	}
-	
+
 	public Feat getFeat(int index) {
 		return feats[index];
 	}
-	
+
 	public void setLevel(int l){
 		lvl = l;
 	}
