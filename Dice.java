9ec1cf0bca19bb72c8126fa54bf81526d922abe7
mattlink <mattlink570@gmail.com>
2017-11-30 14:47:01 -0600

    added documentation for Dice.java

--- Dice.java
+++ Dice.java
@@ -1,9 +1,17 @@
-
+/**
+ * Dice.java
+ * Used to simulate dicerolls
+ */
 public class Dice {
 	public Dice() {
-		
+
 	}
 
+	/**
+	 * roll
+	 * @param diceRoll: String of the number of sides the die should have
+	 * @return total: the value of the rolled dice
+	 */
 	public int roll(String diceRoll) {
 		String dice = diceRoll;
 		int total = 0;
@@ -15,7 +23,7 @@ public class Dice {
 		else if(Integer.parseInt(dice.substring(0,dice.indexOf('d'))) > 1) {
 			int amt = Integer.parseInt(dice.substring(0,dice.indexOf('d')));
 			total += roll((amt-1)+dice.substring(dice.indexOf('d')));
-			
+
 		}
 		if(dice.indexOf('+') == -1)
 			total += (int)(Math.random()*(Integer.parseInt(dice.substring(dice.indexOf('d')+1)))+1);
