26ae0e56820469e019e88dba0ff2ddb33cea9f1f
WestonChan <westonchan.888@hotmail.com>
2017-11-29 20:52:24 -0600

    Add files via upload

--- CharacterSheet.java
+++ CharacterSheet.java
@@ -9,6 +9,7 @@ public class CharacterSheet {
 	private Spell[] spells;
 	private Feat[] feats;
 	private ArrayList<Item> items;
+	private int maxHealth;
 	private int exp;
 	private int lvl;
 	private int str;
@@ -18,7 +19,7 @@ public class CharacterSheet {
 	private int wis;
 	private int cha;
 	
-	public CharacterSheet(String n, Race r, Class[] c, Spell[] s, Feat[] f, ArrayList<Item> i, int exp, int l, int str, int dex, int con, int inte, int wis, int cha) {
+	public CharacterSheet(String n, Race r, Class[] c, Spell[] s, Feat[] f, ArrayList<Item> i, int exp, int l, int str, int dex, int con, int inte, int wis, int cha, int max) {
 		name = n;
 		race = r;
 		classes = c;
@@ -33,6 +34,25 @@ public class CharacterSheet {
 		intelligence = inte;
 		this.wis = wis;
 		this.cha = cha;
+		maxHealth = max;
+	}
+	public CharacterSheet(String n, Race r, Class[] c, Spell[] s, Feat[] f, ArrayList<Item> i, int max) {
+		name = n;
+		race = r;
+		classes = c;
+		spells = s;
+		feats = f;
+		items = i;
+		exp = 0;
+		lvl = 1;
+		Dice d = new Dice();
+		str = d.roll("3d6") + race.getStrMod() + classes[0].getStrMod();
+		dex = d.roll("3d6") + race.getDexMod() + classes[0].getDexMod();
+		con = d.roll("3d6") + race.getConMod() + classes[0].getConMod();
+		intelligence = d.roll("3d6") + race.getIntMod() + classes[0].getIntMod();
+		wis = d.roll("3d6") + race.getWisMod() + classes[0].getWisMod();
+		cha = d.roll("3d6") + race.getChaMod() + classes[0].getChaMod();
+		maxHealth = max;
 	}
 	
 	public String getName() {
