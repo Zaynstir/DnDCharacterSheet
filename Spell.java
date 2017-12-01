3f37af23ec6413ef8babf5f47dad5c4db0dee643
Zaynstir <Zaynstir@gmail.com>
2017-12-01 01:08:33 -0600

    Add files via upload

--- Spell.java
+++ Spell.java
@@ -4,37 +4,31 @@
  */
 public class Spell {
 	final private String name;
-	final private String ability;
-	final private int damage;
-	final private int duration;
-	final private int radius;
-	final private int DC;
-	final private boolean concentration;
-	final private int concentrationDC;
 	final private int lvl;
-
+	final private String action;
+	final private String range;
+	final private String[] components;
+	final private String duration;
+	final private String[] spellList;
+	
 	/**
-	 * Spell() 9-arg constructor
-	 * @param n: name of the spell
-	 * @param a: ability of the spell
-	 * @param dmg: amount of damage that this spell can do
-	 * @param dur: duration of the spell i.e. how many turns the spell is active
-	 * @param rad: radius of the spell, effective area of the spell
-	 * @param dc: the DC value of the spell
-	 * @param con: boolean for if this spell requires concentration
-	 * @param conDC: the DC value for the spell while concentrating
-	 * @param l: level that the user must be to use the spell
+	 * Sets variables that are passed in
+	 * @param n Spell's name
+	 * @param l Spell's lvl
+	 * @param a What it takes to prep a spell
+	 * @param r Spell's range
+	 * @param c Spell's components
+	 * @param d Spell's Duration
+	 * @param s What list it is a part of.
 	 */
-	public Spell(String n, String a, int dmg, int dur, int rad, int dc, boolean con, int conDC, int l) {
+	public Spell(String n, int l, String a, String r, String[] c, String d, String[] s) {
 		name = n;
-		ability = a;
-		damage = dmg;
-		duration = dur;
-		radius = rad;
-		DC = dc;
-		concentration = con;
-		concentrationDC = conDC;
 		lvl = l;
+		action = a;
+		range = r;
+		components = c;
+		duration = d;
+		spellList = s;
 	}
 
 	/**
@@ -44,68 +38,48 @@ public class Spell {
 	public String getName() {
 		return name;
 	}
-
-	/**
-	 * getAbility
-	 * @return ability: the ability attribute of the spell
-	 */
-	public String getAbility() {
-		return ability;
-	}
-
-	/**
-	 * getDamage
-	 * @return damage: the amount of damage that this spell can do
-	 */
-	public int getDamgage() {
-		return damage;
-	}
-
-	/**
-	 * getDuration
-	 * @return duration: the duration of the spell, how many turns it lasts
-	 */
-	public int getDuration() {
-		return duration;
-	}
-
-	/**
-	 * getRadius
-	 * @return radius: how many board units this spell is allowed to reach
-	 */
-	public int getRadius() {
-		return radius;
-	}
-
-	/**
-	 * getDC
-	 * @return DC: the DC value of the spell
-	 */
-	public int getDC() {
-		return DC;
-	}
-
-	/**
-	 * getConcentration
-	 * @return concentration: whether or not the spell requires concentration
-	 */
-	public boolean getConcentration() {
-		return concentration;
-	}
-
-	/**
-	 * getConcentrationDC
-	 * @return concentrationDC: the DC value for the spell when concentrating
-	 */
-	public int getConcentrationDC() {
-		return concentrationDC;
-	}
-
+	
 	/**
 	 * getLvl
 	 * @return lvl: the level required to use this spell
 	 */
 	public int getLvl() {
 		return lvl;
+	}
+	
+	public String getAction() {
+		return action;
+	}
+
+	/**
+	 * getRange
+	 * @return range: how many board units this spell is allowed to reach
+	 */
+	public String getRange() {
+		return range;
+	}
+	
+	/**
+	 * getDuration
+	 * @return duration: the duration of the spell, how many turns it lasts
+	 */
+	public String getDuration() {
+		return duration;
+	}
+	
+	/**
+	 * getComponents()
+	 * @return components: the items needed to make the spell.
+	 */
+	public String[] getComponents() {
+		return components;
+	}
+
+	/**
+	 * getSpellList()
+	 * @return spellList: What spell list they are from
+	 */
+	public String[] getSpellList() {
+		return spellList;
 	}
 }
